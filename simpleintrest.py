ac2a9994a0a7a38439f33d1fee4f6261477d7c5e
Zangetsu01 <gaganbongale@gmail.com>
2025-10-04 09:22:50 +0530

    Create simpleintrest.py

---
+++ simpleintrest.py
@@ -0,0 +1,9 @@
+
+p = float(input("Enter Principal amount: "))
+r = float(input("Enter Rate of interest: "))
+t = float(input("Enter Time (in years): "))
+
+
+si = (p * r * t) / 100
+
+print("Simple Interest =", si)
