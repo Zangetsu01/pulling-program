774b9a221f861c2b1fb78b62117c66fb68b0b973
Zangetsu01 <gaganbongale@gmail.com>
2025-10-04 09:26:55 +0530

    Update simpleintrest.py

--- simpleintrest.py
+++ simpleintrest.py
@@ -6,4 +6,12 @@ t = float(input("Enter Time (in years): "))
 
 si = (p * r * t) / 100
 
+
+a = p * (1 + r/100) ** t
+ci = a - p
+
+
+print("\nResults:")
 print("Simple Interest =", si)
+print("Compound Interest =", ci)
+print("Total Amount with Compound Interest =", a)
